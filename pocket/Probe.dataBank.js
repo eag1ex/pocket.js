56a30bb4a8cea981d38bc93c1b32b80bab377c2d
Your Name <you@example.com>
2021-03-19 14:41:36 +0000

    new build

--- pocket/Probe.dataBank.js
+++ pocket/Probe.dataBank.js
@@ -2,12 +2,14 @@
 * Probe databank extention   
 * 
 **/
-const { v4 } = require('uuid')
+
 const { uniqBy } = require('x-utils-es/umd')
 class ProbeDataBank {
-    _dataBank = []
+   
     constructor() {
+        this._dataBank = []
     }
+
     /**
      * accumulate data into historic array
      * this data is never cleared, unless we unset protected var
@@ -15,7 +17,7 @@ class ProbeDataBank {
      * @returns {*} [{data,data_id},...]
      */
     set dataBank(v) {
-        this._dataBank.push({ data: v, data_id: v4() })
+        this._dataBank.push({ data: v, data_id: new Date().getTime().toString() })
         this._dataBank = this._dataBank.filter(n => n.data !== undefined)
     }
 
