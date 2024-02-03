7147d9117677fa02f95db06d39e783763a1052e1
ex, michael <eag1ex@protonmail.com>
2024-02-03 15:31:33 +0700

    update export types

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 exports.Pocket = require("./libs/Pocket/PocketExit.module")
-exports.PocketUmd = require("./build/js/pocket_umd")
 exports.Probe = require("./libs/Probe/Probe")
 exports.utils = require("./libs/utils")
--- types/index.d.ts
+++ types/index.d.ts
@@ -4,11 +4,14 @@
 
 import type _PocketSelectors from "../libs/Pocket/Pocket.selectors"
 import type _Pocket from "../libs/Pocket/PocketExit.module"
-
+import type _Probe from "../libs/Probe/Probe"
 export interface PocketType {
     new (): _Pocket
 }
 
+export type Pocket = new _Pocket 
+export type Probe  = new _Probe
 export namespace PocketSpace {
     export class PocketSelectors extends _PocketSelectors {}
 }
+
